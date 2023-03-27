b1ebc0e464b8a6a56cd10b4b6feb763f7f1b11ee
Jocelyn Baez <JocelynBaez105@gmail.com>
2023-03-27 13:37:32 -0500

    Initial Commit

---
+++ Landmarks1/Landmarks1App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks1App.swift
+//  Landmarks1
+//
+//  Created by Baez, Jocelyn on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
